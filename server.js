3a4a1eadb31d372f62671080ed28df81d57bcaa0
Magdalena arti Zachara <magdalena.j.zachara@gmail.com>
2019-06-21 10:58:38 +0200

    Add hello world nad procfile

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose"),
+  express = require("express"),
+  app = express(),
+  PORT = process.env.PORT || 5000;
+
+// --- hello wolrld to be displayed on heroku
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 // --- use native promise in Node (default mpromise deprecated)
 mongoose.Promise = global.Promise;
